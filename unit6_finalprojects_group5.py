0b544fddbda1846917f98080a2877d9b8692d52c
Mahia-Chowdhury <71103871+Mahia-Chowdhury@users.noreply.github.com>
2020-10-13 18:26:03 -0400

    Updated File with inputs

--- unit6_finalprojects_group5.py
+++ unit6_finalprojects_group5.py
@@ -39,15 +39,20 @@ stateAbbreviationList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI",
 "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
 df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
-
+Ask = True 
+if Ask == True:
+  Cases_state = input("would you like to see the COVID cases of a specific state, enter 'yes' or 'no'?").lower()
+  deaths_state = input("would you like to see the COVID deaths  of a specific state, enter 'yes' or 'no'?").lower()
+  National 
 """Use the New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create **two functions** that takes the name of a state as input and returns the bar plots of 1) # of cases over time and 2) # of deaths over time for that specific state."""
 state = input("what state's COVID-19 cases would you like to see?")
 # def covid19(s):
 df_State = df.loc[df["state"]== state]
 df_data = df_State.loc[:,["date", "cases"]]
 fig = plt.figure()                
-ax = fig.add_axes([0, 0, 2, 2]) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
+# ax = fig.add_axes([0, 0, 2, 2]) #it was too big, so chnage up the numbers .1,.1,.7.7 #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
 #[starting point x, starting point y, length, width]
+ax = fig.add_subplot(111)
 dates = df_data["date"] #x values
 data_1 = df_data["cases"] #y values 
 
@@ -55,30 +60,32 @@ plt.plot(dates, data_1) #line graph
 plt.bar(dates, data_1) #bar graph
 plt.xlabel("Dates")
 plt.ylabel("Cases")
-ax.xaxis.set_major_locator(plt.MaxNLocator(5))#displays that many ticks evenly
+ax.xaxis.set_major_locator(plt.MaxNLocator(6))#displays that many ticks evenly
 plt.title("Cases of COVID-19 Over Time in " + state)
 plt.plot()
-plt.bar(height=.8, x=.8)
-# plt.show() 
-fig.write_html("fig.html", auto_open=True)
+plt.bar(height= 1, x = 1)
+plt.show() 
+# fig.write_html("fig.html", auto_open=True)
 
-def covid19_deaths(state):
-  state = input("what state's COVID-19 deaths would you like to see?")
-  df_State = df.loc[df["state"]== state]
-  df_data = df_State.loc[:,["date", "deaths"]]
-  fig = plt.figure()                
-  # ax = fig.add_axes([0, 0, 2, 2]) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
+
+state_d = input("what state's COVID-19 deaths would you like to see?")
+df_State = df.loc[df["state"]== state]
+df_data = df_State.loc[:,["date", "deaths"]]
+fig = plt.figure()                
+# ax = fig.add_axes([0, 0, 2, 2]) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
 #[starting point x, starting point y, length, width]
-  dates = df_data["date"] #x values
-  data_1 = df_data["deaths"] #y values 
-
-  plt.plot(dates, data_1) #line graph
-  plt.bar(dates, data_1) #bar graph
-  plt.xlabel("Dates")
-  plt.ylabel("Deaths")
-  plt.title("Deaths from COVID-19 Over Time in " + state)
-  ax.xaxis.set_major_locator(plt.MaxNLocator(5))
-  return plt.plot and plt.bar and  plt.show()
+dates = df_data["date"] #x values
+data_1 = df_data["deaths"] #y values 
+ax = fig.add_subplot(111)
+plt.plot(dates, data_1) #line graph
+plt.bar(dates, data_1) #bar graph
+plt.xlabel("Dates")
+plt.ylabel("Deaths")
+plt.title("Deaths from COVID-19 Over Time in " + state)
+ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+plt.plot()
+plt.bar(height= 1,x = 1)
+plt.show()
 
 """Call the functions on your assigned state to visualize the data."""
 
@@ -87,68 +94,67 @@ def covid19_deaths(state):
 """Use New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create a choropleth map of the United States based on its numbers of COVID-19 cases and deaths. (Use colors that you think are appropriate)."""
 
 #Dataframe Formatter
-def dfFormatter(df, selectColumn):
-    df = df
-    selectColumn = input("would you like to see the cases or deaths?").lower()
-    #Separate df_formatted from df. Create list of states and their latitudes and longitudes.
-    df_formatted = pd.DataFrame(statesList, columns = ['state']) #a colomun of states
-    df_formatted['lat'] = latitudes
-    df_formatted['long'] = longitudes
-    
-    #Create columns for each date, start off by zeroing out everything.
-    #previousDate = datetime.datetime(2020, 1, 1) #Commented out because datetime is no longer a type.
-    previousDate = "2020-01-01" #goes through the date column in org df, a starting point,if it is not equal to jan 1st,2020, it zeroes out evrything 
-    for i in range(len(df['date'])):
-        if df['date'][i] != previousDate:
-            df_formatted[df['date'][i]] = 0
 
-    #Set index to states temporarily.
-    df_formatted.set_index('state', inplace=True) #instead of making the index numbers, it makes it states
+df = df
+selectColumn = input("would you like to see the cases or deaths?").lower()
+#Separate df_formatted from df. Create list of states and their latitudes and longitudes.
+df_formatted = pd.DataFrame(statesList, columns = ['state']) #a colomun of states
+df_formatted['lat'] = latitudes
+df_formatted['long'] = longitudes
 
-    
-    #Select which data you want to use based on the column that you are plotting (cases or deaths)
-    if selectColumn == 'cases':
-        #Get number of cases from df and put them in df_formatted.  
-        for i in range(len(df['state'])):
-            df_formatted.loc[df['state'][i], df['date'][i]] = df['cases'][i]
-            #Format for .loc : df_formatted.loc['index/row label', 'column label'] = 'value'
-    elif selectColumn == 'deaths':
-        for i in range(len(df['state'])):
-            df_formatted.loc[df['state'][i], df['date'][i]] = df['deaths'][i]
-            #Format for .loc : df_formatted.loc['index/row label', 'column label'] = 'value'
+#Create columns for each date, start off by zeroing out everything.
+#previousDate = datetime.datetime(2020, 1, 1) #Commented out because datetime is no longer a type.
+previousDate = "2020-01-01" #goes through the date column in org df, a starting point,if it is not equal to jan 1st,2020, it zeroes out evrything 
+for i in range(len(df['date'])):
+if df['date'][i] != previousDate:
+  df_formatted[df['date'][i]] = 0
 
-    
-    #Reset index when done so that state becomes a column without being the index.
-    df_formatted.reset_index(inplace=True)
-    
-    #Drop last 5 rows because we are focusing on the 50 states.
-    df_formatted.drop([50,51,52,53,54], inplace=True)
-        
-    return df_formatted
+#Set index to states temporarily.
+df_formatted.set_index('state', inplace=True) #instead of making the index numbers, it makes it states
+
+
+#Select which data you want to use based on the column that you are plotting (cases or deaths)
+if selectColumn == 'cases':
+#Get number of cases from df and put them in df_formatted.  
+for i in range(len(df['state'])):
+  df_formatted.loc[df['state'][i], df['date'][i]] = df['cases'][i]
+  #Format for .loc : df_formatted.loc['index/row label', 'column label'] = 'value'
+elif selectColumn == 'deaths':
+for i in range(len(df['state'])):
+  df_formatted.loc[df['state'][i], df['date'][i]] = df['deaths'][i]
+  #Format for .loc : df_formatted.loc['index/row label', 'column label'] = 'value'
+
+
+#Reset index when done so that state becomes a column without being the index.
+df_formatted.reset_index(inplace=True)
+
+#Drop last 5 rows because we are focusing on the 50 states.
+df_formatted.drop([50,51,52,53,54], inplace=True)
+
 
 #Call the function and assign what it returns to the dataframe variables you will be using.
 
 
-def d_c(date, dc): #dc = deaths or cases
-  df_dc = dfFormatter(df, dc)
-  date = input("what date would you like to choose in the format y-m-d?")
-  dc = input("would you like a visualization on the deaths or cases").lower()
-  colors = ["#FF4D00", "#FF6400", "#FF7800", "#FF8B00",  "#FF9E00", "#FFAE00", "#FFD800",
-          "#FFE800 ", "#FFF700", "#E8FF00", "#D8FF00",  "#C1FF00", "#B2FF00", "#8BFF00",
-          "#2EFF00", "#1BFF00", "#17FF00", "#0CFF00",  "#00FF0C", "#00FF2A", "#00FF3E" ]
-  fig = go.Figure(data = go.Choropleth(
-   locationmode = "USA-states", 
-   locations = stateAbbreviationList,
-   z = df_dc[date], #z repsents the color assignments,  the location, and what data you are accessing
-   colorscale = colors,
-   reversescale = False,
-   autocolorscale = False #gives default patterns
+# def d_c(date, dc): #dc = deaths or cases
+df_dc = dfFormatter(df, dc)
+date = input("what date would you like to choose in the format y-m-d?")
+dc = input("would you like a visualization on the deaths or cases").lower()
+colors = ["#FF4D00", "#FF6400", "#FF7800", "#FF8B00",  "#FF9E00", "#FFAE00", "#FFD800",
+"#FFE800 ", "#FFF700", "#E8FF00", "#D8FF00",  "#C1FF00", "#B2FF00", "#8BFF00",
+"#2EFF00", "#1BFF00", "#17FF00", "#0CFF00",  "#00FF0C", "#00FF2A", "#00FF3E" ]
+fig = go.Figure(data = go.Choropleth(
+locationmode = "USA-states", 
+locations = stateAbbreviationList,
+z = df_dc[date], #z repsents the color assignments,  the location, and what data you are accessing
+colorscale = colors,
+reversescale = False,
+autocolorscale = False #gives default patterns
 #array = list 
-  ))
-  fig.update_layout(
-     title_text = "Number of COVID-19 " + dc + " in USA",
-     geo = dict(showcoastlines = True, scope = "usa"))
-  fig.show()
+))
+fig.update_layout(
+title_text = "Number of COVID-19 " + dc + " in USA",
+geo = dict(showcoastlines = True, scope = "usa"))
+fig.show()
 
 
 
