17c942b1cac9dba0fcae4c0e03fc2f8e162eabdb
Mahia-Chowdhury <71103871+Mahia-Chowdhury@users.noreply.github.com>
2020-10-15 16:56:20 -0400

    still needs work

--- unit6_finalprojects_group5.py
+++ unit6_finalprojects_group5.py
@@ -1,99 +1,99 @@
 
 
-# Commented out IPython magic to ensure Python compatibility.
-#Import all libraries you may need in this cell:
-import pandas as pd 
-import plotly.graph_objects as go 
-import matplotlib.pyplot as plt 
-#%matplotlib inline
+# # Commented out IPython magic to ensure Python compatibility.
+# #Import all libraries you may need in this cell:
+# import pandas as pd 
+# import plotly.graph_objects as go 
+# import matplotlib.pyplot as plt 
+# #%matplotlib inline
 
-"""## 1 - Finding the COVID-19 numbers per state."""
+# """## 1 - Finding the COVID-19 numbers per state."""
 
-#3 lists of data compiled for your convenience
+# #3 lists of data compiled for your convenience
 
-#List of states sorted in alphabetical order.
-statesList = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
-  "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
-  "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
-  "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
-  "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
-  "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
-  "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
-  "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+# #List of states sorted in alphabetical order.
+# statesList = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+#   "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
+#   "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
+#   "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
+#   "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
+#   "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
+#   "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
+#   "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
 
-#Corresponding latitudes for each state.
-latitudes = [32.361538, 58.301935, 33.448457, 34.736009, 38.555605, 39.7391667, 41.767, 39.161921, 30.4518, 33.76,
-            21.30895, 43.613739, 39.783250, 39.790942, 41.590939, 39.04, 38.197274, 30.45809, 44.323535, 38.972945,
-            42.2352, 42.7335, 44.95,32.320, 38.572954,46.595805,40.809868,39.160949,43.220093, 40.221741,35.667231,
-            42.659829,35.771, 48.813343,39.962245, 35.482309,44.931109, 40.269789, 41.82355, 34.000,  44.367966,36.165,
-            30.266667,40.7547,44.26639, 37.54, 47.042418,38.349497,43.074722, 41.145548]
+# #Corresponding latitudes for each state.
+# latitudes = [32.361538, 58.301935, 33.448457, 34.736009, 38.555605, 39.7391667, 41.767, 39.161921, 30.4518, 33.76,
+#             21.30895, 43.613739, 39.783250, 39.790942, 41.590939, 39.04, 38.197274, 30.45809, 44.323535, 38.972945,
+#             42.2352, 42.7335, 44.95,32.320, 38.572954,46.595805,40.809868,39.160949,43.220093, 40.221741,35.667231,
+#             42.659829,35.771, 48.813343,39.962245, 35.482309,44.931109, 40.269789, 41.82355, 34.000,  44.367966,36.165,
+#             30.266667,40.7547,44.26639, 37.54, 47.042418,38.349497,43.074722, 41.145548]
 
-#Corresponding longitudes for each state.
-longitudes = [-86.279118,-134.419740,-112.073844, -92.331122,-121.468926 ,-104.984167 ,-72.677 , -75.526755 ,
-              -84.27277,-84.39,-157.826182 ,-116.237651 ,-89.650373 ,-86.147685 ,-93.620866 ,-95.69 , -84.86311
-              ,-91.140229 ,-69.765261 ,-76.501157 ,-71.0275 ,-84.5467 , -93.094 ,-90.207 , -92.189283
-              , -112.027031 ,-96.675345 ,-119.753877 ,-71.549127 , -74.756138, -105.964575 , -73.781339 ,-78.638 
-              ,-100.779004 ,-83.000647 , -97.534994,-123.029159 ,-76.875613 , -71.422132, -81.035, -100.336378
-              ,-86.784 ,-97.75 ,-111.892622 ,-72.57194 ,-77.46 ,-122.893077 ,-81.633294 ,-89.384444 ,-104.802042]
-stateAbbreviationList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
-"OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+# #Corresponding longitudes for each state.
+# longitudes = [-86.279118,-134.419740,-112.073844, -92.331122,-121.468926 ,-104.984167 ,-72.677 , -75.526755 ,
+#               -84.27277,-84.39,-157.826182 ,-116.237651 ,-89.650373 ,-86.147685 ,-93.620866 ,-95.69 , -84.86311
+#               ,-91.140229 ,-69.765261 ,-76.501157 ,-71.0275 ,-84.5467 , -93.094 ,-90.207 , -92.189283
+#               , -112.027031 ,-96.675345 ,-119.753877 ,-71.549127 , -74.756138, -105.964575 , -73.781339 ,-78.638 
+#               ,-100.779004 ,-83.000647 , -97.534994,-123.029159 ,-76.875613 , -71.422132, -81.035, -100.336378
+#               ,-86.784 ,-97.75 ,-111.892622 ,-72.57194 ,-77.46 ,-122.893077 ,-81.633294 ,-89.384444 ,-104.802042]
+# stateAbbreviationList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
+# "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
-df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
-Ask = True 
-if Ask == True:
-  Cases_state = input("would you like to see the COVID cases of a specific state, enter 'yes' or 'no'?").lower()
-  deaths_state = input("would you like to see the COVID deaths  of a specific state, enter 'yes' or 'no'?").lower()
-  National 
-"""Use the New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create **two functions** that takes the name of a state as input and returns the bar plots of 1) # of cases over time and 2) # of deaths over time for that specific state."""
-state = input("what state's COVID-19 cases would you like to see?")
-# def covid19(s):
-df_State = df.loc[df["state"]== state]
-df_data = df_State.loc[:,["date", "cases"]]
-fig = plt.figure()                
-# ax = fig.add_axes([0, 0, 2, 2]) #it was too big, so chnage up the numbers .1,.1,.7.7 #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
-#[starting point x, starting point y, length, width]
-ax = fig.add_subplot(111)
-dates = df_data["date"] #x values
-data_1 = df_data["cases"] #y values 
+# df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+# Ask = True 
+# if Ask == True:
+#   Cases_state = input("would you like to see the COVID cases of a specific state, enter 'yes' or 'no'?").lower()
+#   deaths_state = input("would you like to see the COVID deaths  of a specific state, enter 'yes' or 'no'?").lower()
+#   National 
+# """Use the New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create **two functions** that takes the name of a state as input and returns the bar plots of 1) # of cases over time and 2) # of deaths over time for that specific state."""
+# state = input("what state's COVID-19 cases would you like to see?")
+# # def covid19(s):
+# df_State = df.loc[df["state"]== state]
+# df_data = df_State.loc[:,["date", "cases"]]
+# fig = plt.figure()                
+# # ax = fig.add_axes([0, 0, 2, 2]) #it was too big, so chnage up the numbers .1,.1,.7.7 #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
+# #[starting point x, starting point y, length, width]
+# ax = fig.add_subplot(111)
+# dates = df_data["date"] #x values
+# data_1 = df_data["cases"] #y values 
 
-plt.plot(dates, data_1) #line graph
-plt.bar(dates, data_1) #bar graph
-plt.xlabel("Dates")
-plt.ylabel("Cases")
-ax.xaxis.set_major_locator(plt.MaxNLocator(6))#displays that many ticks evenly
-plt.title("Cases of COVID-19 Over Time in " + state)
-plt.plot()
-plt.bar(height= 1, x = 1)
-plt.show() 
-# fig.write_html("fig.html", auto_open=True)
-
-
-state_d = input("what state's COVID-19 deaths would you like to see?")
-df_State = df.loc[df["state"]== state]
-df_data = df_State.loc[:,["date", "deaths"]]
-fig = plt.figure()                
-# ax = fig.add_axes([0, 0, 2, 2]) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
-#[starting point x, starting point y, length, width]
-dates = df_data["date"] #x values
-data_1 = df_data["deaths"] #y values 
-ax = fig.add_subplot(111)
-plt.plot(dates, data_1) #line graph
-plt.bar(dates, data_1) #bar graph
-plt.xlabel("Dates")
-plt.ylabel("Deaths")
-plt.title("Deaths from COVID-19 Over Time in " + state)
-ax.xaxis.set_major_locator(plt.MaxNLocator(6))
-plt.plot()
-plt.bar(height= 1,x = 1)
-plt.show()
-
-"""Call the functions on your assigned state to visualize the data."""
+# plt.plot(dates, data_1) #line graph
+# plt.bar(dates, data_1) #bar graph
+# plt.xlabel("Dates")
+# plt.ylabel("Cases")
+# ax.xaxis.set_major_locator(plt.MaxNLocator(6))#displays that many ticks evenly
+# plt.title("Cases of COVID-19 Over Time in " + state)
+# plt.plot()
+# plt.bar(height= 1, x = 1)
+# plt.show() 
+# # fig.write_html("fig.html", auto_open=True)
 
 
+# state_d = input("what state's COVID-19 deaths would you like to see?")
+# df_State = df.loc[df["state"]== state]
+# df_data = df_State.loc[:,["date", "deaths"]]
+# fig = plt.figure()                
+# # ax = fig.add_axes([0, 0, 2, 2]) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
+# #[starting point x, starting point y, length, width]
+# dates = df_data["date"] #x values
+# data_1 = df_data["deaths"] #y values 
+# ax = fig.add_subplot(111)
+# plt.plot(dates, data_1) #line graph
+# plt.bar(dates, data_1) #bar graph
+# plt.xlabel("Dates")
+# plt.ylabel("Deaths")
+# plt.title("Deaths from COVID-19 Over Time in " + state)
+# ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+# plt.plot()
+# plt.bar(height= 1,x = 1)
+# plt.show()
 
-"""Use New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create a choropleth map of the United States based on its numbers of COVID-19 cases and deaths. (Use colors that you think are appropriate)."""
+# """Call the functions on your assigned state to visualize the data."""
 
-#Dataframe Formatter
+
+
+# """Use New York Times' COVID-19 data(https://github.com/nytimes/covid-19-data/blob/master/us-states.csv) to create a choropleth map of the United States based on its numbers of COVID-19 cases and deaths. (Use colors that you think are appropriate)."""
+
+# #Dataframe Formatter
 
 df = df
 selectColumn = input("would you like to see the cases or deaths?").lower()
@@ -106,7 +106,7 @@ df_formatted['long'] = longitudes
 #previousDate = datetime.datetime(2020, 1, 1) #Commented out because datetime is no longer a type.
 previousDate = "2020-01-01" #goes through the date column in org df, a starting point,if it is not equal to jan 1st,2020, it zeroes out evrything 
 for i in range(len(df['date'])):
-if df['date'][i] != previousDate:
+  if df['date'][i] != previousDate:
   df_formatted[df['date'][i]] = 0
 
 #Set index to states temporarily.
