812a5f3ca1350f4f65215dab3aa4355931bdb369
Brown Scholar <brownscholar@educ-lap-507.home>
2020-10-27 18:10:06 -0400

    added user input and typo correction

--- covid_graph.py
+++ covid_graph.py
@@ -28,40 +28,67 @@ longitudes = [-86.279118,-134.419740,-112.073844, -92.331122,-121.468926 ,-104.9
 
 df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
-def covid19(state):
+def typo(word): 
+  placeholder = ""
+  for i in word: 
+    placeholder = i 
+  if placeholder == " ":
+    fixedword = word[0:len(word) -1]
+    return fixedword
+  return word
+    
+
+def plot(data,state):
   df_State = df.loc[df["state"]== state]
-  df_data = df_State.loc[:,["date", "cases"]]
+  df_data = df_State.loc[:,["date", data]]
   fig = plt.figure()                
   ax = fig.add_subplot(111) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
 #[starting point x, starting point y, length, width]
   dates = df_data["date"] #x values
-  data_1 = df_data["cases"] #y values 
+  data_1 = df_data[data] #y values 
 
   plt.plot(dates, data_1) #line graph
   plt.bar(dates, data_1) #bar graph
   plt.xlabel("Dates")
-  plt.ylabel("Cases")
+  plt.ylabel(data)
   ax.xaxis.set_major_locator(plt.MaxNLocator(5))#displays that many ticks evenly
-  plt.title("Cases of COVID-19 Over Time in " + state)
+  plt.title(data + " of COVID-19 Over Time in " + state)
 
  
   return plt.plot and plt.bar and  plt.show()
-covid19("New York")    
 
-def covid19_deaths(state):
-  df_State = df.loc[df["state"]== state]
-  df_data = df_State.loc[:,["date", "deaths"]]
-  fig = plt.figure()                
-  ax = fig.add_subplot(111) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
+interested = True 
+while interested == True:
+    data = input("Are you interested in seeing Covid-19 cases or deaths?").lower() #variable for cases or deaths 
+    state = input("What state do you want to see Covid-19 " + data + " for?") # variable for state
+    fixeddata = typo(data)
+    fixedstate = typo(state)
+    plot(fixeddata,fixedstate)
+    Continue = input("Would you like to keep investigating? please type 'yes' or 'no' ")
+    if Continue.lower() == "yes": 
+     interested = True 
+    else:
+     interested = False
+     break
+
+
+
+#covid19("New York")    
+
+#def covid19_deaths(state):
+  #df_State = df.loc[df["state"]== state]
+ # df_data = df_State.loc[:,["date", "deaths"]]
+  #fig = plt.figure()                
+  #ax = fig.add_subplot(111) #add_axes is amethod that takes a list of 4 number where you wanna start x-axis, whre you wanna start on y-axis, and the length and width of figure 
 #[starting point x, starting point y, length, width]
-  dates = df_data["date"] #x values
-  data_1 = df_data["deaths"] #y values 
+  #dates = df_data["date"] #x values
+  #data_1 = df_data["deaths"] #y values 
 
-  plt.plot(dates, data_1) #line graph
-  plt.bar(dates, data_1) #bar graph
-  plt.xlabel("Dates")
-  plt.ylabel("Deaths")
-  plt.title("Deaths from COVID-19 Over Time in " + state)
-  ax.xaxis.set_major_locator(plt.MaxNLocator(5))
-  return plt.plot and plt.bar and  plt.show()
-covid19_deaths("New York")
+  #plt.plot(dates, data_1) #line graph
+  #plt.bar(dates, data_1) #bar graph
+  #plt.xlabel("Dates")
+  #plt.ylabel("Deaths")
+  #plt.title("Deaths from COVID-19 Over Time in " + state)
+  #ax.xaxis.set_major_locator(plt.MaxNLocator(5))
+  #return plt.plot and plt.bar and  plt.show()
+#covid19_deaths("New York")
