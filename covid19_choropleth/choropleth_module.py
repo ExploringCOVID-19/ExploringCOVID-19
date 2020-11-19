908db5cbc6be9cf2d6f3770019e6739c93f535fd
Dnamicel04 <dminina04@gmail.com>
2020-11-19 18:36:26 -0500

    adding my work; in a folder

--- covid19_choropleth/choropleth_module.py
+++ covid19_choropleth/choropleth_module.py
@@ -1,16 +1,16 @@
-#Import all libraries you may need in this cell:
-#Import all libraries you may need in this cell:
- import numpy as np # used to arrange x-axis values for bar plot
- import matplotlib.pyplot as plt #to make a visualization
- import pandas as pd #to read in csv files and create dataframes
- import plotly.graph_objects as go # to create a colorful map visualization 
- from matplotlib.dates import DateFormatter #to use matplotlib's date formatter
- # %matplotlib inline 
- # ^ sets the backend of matplotlib to the 'inline' backend.
- # With this backend, the output of plotting commands is displayed inline within frontends like 
- # the Jupyter notebook, directly below the code cell that produced it.
+import numpy as np # used to arrange x-axis values for bar plot
+import matplotlib.pyplot as plt #to make a visualization
+import pandas as pd #to read in csv files and create dataframes
+import plotly.graph_objects as go # to create a colorful map visualization 
+from matplotlib.dates import DateFormatter #to use matplotlib's date formatter
+# %matplotlib inline 
+# ^ sets the backend of matplotlib to the 'inline' backend.
+# With this backend, the output of plotting commands is displayed inline within frontends like 
+# the Jupyter notebook, directly below the code cell that produced it.
 
- statesList = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
+nyt_df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+
+statesList = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
    "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
    "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
    "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
@@ -19,29 +19,28 @@
    "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
    "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
 
- #List of state abbreviations sorted in alphabetical order.
- stateAbbreviationList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
+ #List of state abbreviations sorted in alphabetical order. s
+stateAbbreviationList = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND",
  "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
  #Corresponding latitudes for each state.
- latitudes = [32.361538, 58.301935, 33.448457, 34.736009, 38.555605, 39.7391667, 41.767, 39.161921, 30.4518, 33.76,
+latitudes = [32.361538, 58.301935, 33.448457, 34.736009, 38.555605, 39.7391667, 41.767, 39.161921, 30.4518, 33.76,
              21.30895, 43.613739, 39.783250, 39.790942, 41.590939, 39.04, 38.197274, 30.45809, 44.323535, 38.972945,
              42.2352, 42.7335, 44.95,32.320, 38.572954,46.595805,40.809868,39.160949,43.220093, 40.221741,35.667231,
              42.659829,35.771, 48.813343,39.962245, 35.482309,44.931109, 40.269789, 41.82355, 34.000,  44.367966,36.165,
              30.266667,40.7547,44.26639, 37.54, 47.042418,38.349497,43.074722, 41.145548]
 
  #Corresponding longitudes for each state.
- longitudes = [-86.279118,-134.419740,-112.073844, -92.331122,-121.468926 ,-104.984167 ,-72.677 , -75.526755 ,
+longitudes = [-86.279118,-134.419740,-112.073844, -92.331122,-121.468926 ,-104.984167 ,-72.677 , -75.526755 ,
                -84.27277,-84.39,-157.826182 ,-116.237651 ,-89.650373 ,-86.147685 ,-93.620866 ,-95.69 , -84.86311
                ,-91.140229 ,-69.765261 ,-76.501157 ,-71.0275 ,-84.5467 , -93.094 ,-90.207 , -92.189283
                , -112.027031 ,-96.675345 ,-119.753877 ,-71.549127 , -74.756138, -105.964575 , -73.781339 ,-78.638 
                ,-100.779004 ,-83.000647 , -97.534994,-123.029159 ,-76.875613 , -71.422132, -81.035, -100.336378
                ,-86.784 ,-97.75 ,-111.892622 ,-72.57194 ,-77.46 ,-122.893077 ,-81.633294 ,-89.384444 ,-104.802042]
 
- nyt_df = pd.read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
- #Dataframe Formatter
- def dfFormatter(df, selectColumn):
+#Dataframe Formatter
+def dfFormatter(df, selectColumn):
      #Separate df_formatted from df. Create list of states and their latitudes and longitudes.
      df_formatted = pd.DataFrame(statesList, columns = ['state'])
      df_formatted['lat'] = latitudes
@@ -78,13 +77,13 @@
 
      return df_formatted
 
- #Call the function and assign what it returns to the dataframe variables you will be using.
- cases = dfFormatter(nyt_df, "cases")
- cases["State abbreviations"] = stateAbbreviationList
- deaths = dfFormatter(nyt_df, "deaths")
- deaths["State abbreviations"] = stateAbbreviationList
+#Call the function and assign what it returns to the dataframe variables you will be using.
+cases = dfFormatter(nyt_df, "cases")
+cases["State abbreviations"] = stateAbbreviationList
+deaths = dfFormatter(nyt_df, "deaths")
+deaths["State abbreviations"] = stateAbbreviationList
 
- def death_map(date): # defines function
+def death_map(date): # defines function
    colors =['#f7fbff','#deebf7','#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#08519c','#08306b'] #Hex values
    fig = go.Figure(data = go.Choropleth( # creates a figure and assigns it to a function that creates a choropleth map
                      locationmode= "USA-states", # determines the set of locations used to match entries in "locations" parameter
@@ -105,7 +104,7 @@
      )
    fig.write_html("deathsperstate.html", auto_open = True)
 
-   def caseFatalityrate_map(date):
+def caseFatalityrate_map(date):
      colors = ['#f7fcf5','#e5f5e0','#c7e9c0','#a1d99b','#74c476','#41ab5d','#238b45','#006d2c','#00441b']
      case_fatalityRate = deaths[date]/cases[date]
      fat_fig = go.Figure(data = go.Choropleth(
@@ -126,11 +125,3 @@
          )
      )
      fat_fig.write_html("casefatrate.html", auto_open = True)
-
- decision = str(input("Type \'A\' to view a map of COVID-19 case fatality rates or \'B\' to view a map of COVID-19 deaths. "))
- if decision is "A":
-   user_caseFat_date = str(input("Which date would you like to view COVID-19 case fatality rates of (YYYY-MM-DD)? "))
-   caseFatalityrate_map(user_caseFat_date)
- elif decision is "B":
-   user_date = str(input("Which date would you like to see COVID-19 deaths for (YYYY-MM-DD)? "))
-   death_map(user_date)
