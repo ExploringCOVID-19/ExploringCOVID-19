83179352eb809492602fef660abd54fa3ea4d108
ChiChi <CheeChizzle@users.noreply.github.com>
2021-04-15 17:38:10 -0400

    made comments and delegated roles

---
+++ fully_vaccinated_model/give_answer_module.py
@@ -0,0 +1,14 @@
+from fbprophet import Prophet
+import pandas as pd 
+import matplotlib.pyplot as plot
+import numpy as np
+
+# function three
+# takes in dataframe (with predictions)
+# find out what is an indicator to lift covid restrictions
+# make conditions/a loop to find the predicted day the data value will meet restriction indicator
+# take varying results into account and take note of likelehoods that are safer than others
+# returns string with date
+
+# lin
+# dina
