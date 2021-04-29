41845a9a142457bd9e8edd242b1359b906aad683
Lin <lfinnegan32@gmail.com>
2021-04-29 17:13:05 -0400

    changed it to have a seperate population module

--- fully_vaccinated_model/give_answer_module.py
+++ fully_vaccinated_model/give_answer_module.py
@@ -1,20 +1,10 @@
 import numpy as np
-import requests
-import json
-
-API = "https://api.census.gov/data/2019/pep/charagegroups?get=NAME,POP&for=us:*&key=f7038b3e8176fc9358413aad840b9ffbc972a142"
-
-#call the API and collect the response
-response = requests.get(API)
-#load the response into a JSON, ignoring the first element which is just field labels
-formattedResponse = json.loads(response.text)[1:]
-population = int(formattedResponse[0][1])
+import API_module
 
 # function three
 def answer(df):                                     # take prediction
-    population = int(formattedResponse[1][1])       # temporary population
-    minGoal = population *0.7                       # 70% of population is realistic goal (fully vaccinated)
-    maxGoal = population *0.9                       # 90% -> unreliastic goal (at the moment)
+    minGoal = API_module.population *0.7            # 70% of population is realistic goal (fully vaccinated)
+    maxGoal = API_module.population *0.9            # 90% -> unreliastic goal (at the moment)
     minIndex = ''                                   # empty placeholders:
     maxIndex = ''                            
     for value in df["y"]:                           # finds y value instance where this is true ^
@@ -22,4 +12,4 @@ def answer(df):                                     # take prediction
             minIndex = df["ds"].index(value)        # returns answer
         elif value == maxGoal: 
             maxIndex = df["ds"].index(value)
-    return minIndex, maxIndex                
+    return minIndex, maxIndex                       
